2559011f0dc58825a1865af8e4cbda60d9247c64
jkolder <jkolder@netways.de>
2023-05-26 10:30:00 +0200

    complete base mine layer, defined layer 3 and 4

--- keyboards/keychron/v3/iso_encoder/keymaps/custom_mine/keymap.c
+++ keyboards/keychron/v3/iso_encoder/keymaps/custom_mine/keymap.c
@@ -19,11 +19,32 @@
 
 // clang-format off
 
+enum unicode_names {
+    ue,
+    UE,
+    oe,
+    OE,
+    ae,
+    AE,
+    SHARP_S
+};
+
+const uint32_t unicode_map[] PROGMEM = {
+    [ue] = 0x00FC, // ü
+    [UE] = 0x00DC, // Ü
+    [oe] = 0x00F6, // ö
+    [OE] = 0x00D6, // Ö
+    [ae] = 0x00E4, // ä
+    [AE] = 0x00C4, // Ä
+    [SHARP_S] = 0x00DF // ß
+};
+
 enum layers{
     QWERTY_BASE,
     QWERTY_FN,
     MINE_BASE,
-    MINE_LAYER2,
+    M_3,
+    M_4,
     MINE_FN
 };
 
@@ -34,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,                KC_DEL,   KC_END,   KC_PGDN,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,    KC_ENT,
         KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,              KC_UP,
-        KC_LCTL,  KC_LCMD,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN, MO(MINE_FN),  KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_LCTL,  KC_LCMD,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN, MO(QWERTY_FN),  KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [QWERTY_FN] = LAYOUT_tkl_f13_iso(
         _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     RGB_TOG,    _______,  _______,  RGB_TOG,
@@ -47,10 +68,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MINE_BASE] = LAYOUT_tkl_f13_iso(
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     KC_MUTE,    KC_PSCR,  KC_CRTA,  RGB_MOD,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     UC(0x00FC),  KC_RBRC,                KC_DEL,   KC_END,   KC_PGDN,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,    KC_ENT,
-        KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,              KC_UP,
-        KC_LCTL,  KC_LCMD,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN, MO(MINE_FN),  KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_TAB,   KC_J,     KC_L,     KC_U,     KC_A,     KC_Q,     KC_W,     KC_B,     KC_D,     KC_G,     KC_Y,     KC_Z,     X(SHARP_S),                KC_DEL,   KC_END,   KC_PGDN,
+        MO(M_3),  KC_C,     KC_R,     KC_I,     KC_E,     KC_O,     KC_M,     KC_N,     KC_T,     KC_S,     KC_H,     MO(M_3),  KC_NUHS,    KC_ENT,
+        KC_LSFT,  MO(M_4),  KC_V,     KC_X,     XP(ue,UE), XP(ae,AE), XP(oe,OE), KC_P,     KC_F,     KC_COMM,  KC_DOT,   KC_K,              KC_RSFT,              KC_UP,
+        KC_LCTL,  KC_LCMD,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN, MO(QWERTY_FN),  KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [MINE_FN] = LAYOUT_tkl_f13_iso(
         _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    RGB_TOG,    _______,  _______,  RGB_TOG,
@@ -60,7 +81,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,              _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,    _______,  _______,  _______),
 
-    [MINE_LAYER3] = LAYOUT_tkl_f13_iso(
+    [M_3] = LAYOUT_tkl_f13_iso(
+        _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    RGB_TOG,    _______,  _______,  RGB_TOG,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,  _______,  _______,
+        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                _______,  _______,  _______,
+        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,              _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,    _______,  _______,  _______),
+
+    [M_4] = LAYOUT_tkl_f13_iso(
         _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    RGB_TOG,    _______,  _______,  RGB_TOG,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,  _______,  _______,
         RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                _______,  _______,  _______,
@@ -77,7 +106,8 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [QWERTY_FN]   = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
     [MINE_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
     [MINE_FN]   = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
-    [MINE_LAYER3]   = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
+    [M_3]   = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [M_4]   = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
 };
 #endif // ENCODER_MAP_ENABLE
 
